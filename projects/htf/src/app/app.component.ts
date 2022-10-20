02b2aa4d7b431775d139db44989929f4ab98033c
Levent Önal <levent.onal@collide.be>
2022-10-20 12:37:45 +0200

    quest html

--- projects/htf/src/app/app.component.ts
+++ projects/htf/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { SpellService } from './spell.service';
 
 @Component({
   selector: 'htf-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'htf';
+  activeSpell?: string;
+  answer?: string;
+
+  @Input() set url(value: string) {
+    this.spellService.url = value;
+  }
+  get url() {
+    return this.spellService.url;
+  }
+  @Input() quest?: any;
+
+  constructor(private spellService: SpellService) {}
+
+  activate(id: string) {
+    this.answer = '';
+    this.activeSpell = id;
+  }
+
+  castSpell(spell: any) {
+    this.spellService.castSpell(spell, this.answer || '');
+  }
 }
