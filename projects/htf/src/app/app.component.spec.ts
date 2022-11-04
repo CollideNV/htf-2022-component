269189161d0569a639be3d6f4897d2581e935509
Levent Önal <levent.onal@collide.be>
2022-11-04 14:50:50 +0100

    fix tests

--- projects/htf/src/app/app.component.spec.ts
+++ projects/htf/src/app/app.component.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { SpellService } from './spell.service';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
+      providers: [{ provide: SpellService, useValue: {} }],
     }).compileComponents();
   });
 
@@ -14,12 +14,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('htf app is running!');
   });
 });
